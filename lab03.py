526a3b41b4afbdb1c3843d125a11a819e1c09d55
LynxGVA <generalgva@gmail.com>
2025-01-24 11:21:35 -0500

    lab03.py

--- lab03.py
+++ lab03.py
@@ -1,1 +1,42 @@
-print("Hello, World!")
+import random
+
+diceOptions = list(range(1,7))
+
+weapons = ["Fist", "Knife", "Club", "Gun", "Bomb","Nuclear Bomb"]
+
+print("Available Weapons: ", ','.join(weapons))
+
+def get_combat_strength(prompt):
+    while True:
+        value = int(input(prompt))
+        if 1 <= value <=6:
+            return value
+        else:
+            print("Invalid input. Please enter a number between 1 and 6.")
+
+combatStrength = get_combat_strength("Enter Hero combat strength (1-6): ")
+mCombatStrength = get_combat_strength("Enter the monster's combat strength (1-6): ")
+
+for j in range(1,21,2):
+    heroRoll = random.choice(diceOptions)
+    monsterRoll = random.choice(diceOptions)
+
+    heroWeapon = weapons[heroRoll-1]
+    monsterWeapon = weapons[monsterRoll-1]
+
+    heroTotal = combatStrength + heroRoll
+    monsterTotal = mCombatStrength + monsterRoll
+
+    print(f"\n Hero rolled {heroRoll}, Monster rolled {monsterRoll}")
+    print(f"\n Hero selected {heroWeapon}, Monster selected {monsterWeapon}")
+    print(f"\n Hero total strength is {heroTotal}, Monster total strength is {monsterTotal}")
+
+    if heroTotal > monsterTotal:
+        print("Hero wins!")
+    elif heroTotal < monsterTotal:
+        print("Monster wins!")
+    else:
+        print("It's a tie!")
+
+if j != 11:
+    print(f"\n Battle cocluded after 20 rounds!")
